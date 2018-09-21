3dd2bd896b91f3b90981baa8e80364035fbe2fd0
Prabhakar Mudliyar <mudliyarp@HCL.COM>
2018-09-21 16:10:15 +0530

    Timesheet reports added

--- protected/views/reports/timesheetreports.php
+++ protected/views/reports/timesheetreports.php
@@ -15,8 +15,8 @@ Yii::app()->clientScript->registerCssFile(
         '/jui/css/base/jquery-ui.css'
 );
 $cs = Yii::app()->getClientScript();
-$cs->registerCssFile("https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css");
-$cs->registerCssFile("https://cdn.datatables.net/buttons/1.5.2/css/buttons.dataTables.min.css");
+$cs->registerCssFile(Yii::app()->baseUrl ."/css/jquery.dataTables.min.css");
+$cs->registerCssFile(Yii::app()->baseUrl ."/css/buttons.dataTables.min.css");
 $cs->registerScriptFile(Yii::app()->baseUrl . "/js/jquery-ui-timepicker-addon.js");
 $cs->registerCssFile(Yii::app()->baseUrl . "/css/jquery-ui-timepicker-addon.css");
 
@@ -100,24 +100,14 @@ $cs->registerCssFile(Yii::app()->baseUrl . "/css/jquery-ui-timepicker-addon.css"
 
 </div>
 
-<?php 
-// $cs->registerScriptFile("https://code.jquery.com/jquery-3.3.1.js");
-// $cs->registerScriptFile("https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js");
-// // $cs->registerScriptFile("https://cdn.datatables.net/buttons/1.5.2/js/buttons.flash.min.js");
-// $cs->registerScriptFile("https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js");
-// $cs->registerScriptFile("https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/pdfmake.min.js");
-// $cs->registerScriptFile("https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/vfs_fonts.js");
-// $cs->registerScriptFile("https://cdn.datatables.net/buttons/1.5.2/js/buttons.html5.min.js");
-// $cs->registerScriptFile("https://cdn.datatables.net/buttons/1.5.2/js/buttons.print.min.js");
-?>
     <!-- <script type="text/javascript" language="javascript" src="https://code.jquery.com/jquery-3.3.1.js"></script> -->
-    <script type="text/javascript" language="javascript" src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
-    <script type="text/javascript" language="javascript" src="https://cdn.datatables.net/buttons/1.5.2/js/dataTables.buttons.min.js"></script>
+    <script type="text/javascript" language="javascript" src="<?php echo Yii::app()->baseUrl; ?>/js/jquery.dataTables.min.js"></script>
+    <script type="text/javascript" language="javascript" src="<?php echo Yii::app()->baseUrl; ?>/js/dataTables.buttons.min.js"></script>
     <!-- <script type="text/javascript" language="javascript" src="https://cdn.datatables.net/buttons/1.5.2/js/buttons.flash.min.js"></script> -->
-    <script type="text/javascript" language="javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+    <script type="text/javascript" language="javascript" src="<?php echo Yii::app()->baseUrl; ?>/js/jszip.min.js"></script>
 
-    <script type="text/javascript" language="javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/vfs_fonts.js"></script>
-    <script type="text/javascript" language="javascript" src="https://cdn.datatables.net/buttons/1.5.2/js/buttons.html5.min.js"></script>
+    <script type="text/javascript" language="javascript" src="<?php echo Yii::app()->baseUrl; ?>/js/vfs_fonts.js"></script>
+    <script type="text/javascript" language="javascript" src="<?php echo Yii::app()->baseUrl; ?>/js/buttons.html5.min.js"></script>
     
 
 
